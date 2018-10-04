5fe7b53578569dcec95d003a0585f709c955fcbc
alishaalihashim <38943237+alishaalihashim@users.noreply.github.com>
2018-10-04 17:54:37 +0500

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+$.ajax({
+	// url: "https://techkaro-test.herokuapp.com/api/v1/getdata",
+	// success : function (data) {
+	// 	console.log(data);
+
+		// document.querySelector(".lotus").innerHTML = `${data.imageURL}`;
+		// document.querySelector(".date").innerHTML = `${data.date}`;
+		// document.querySelector(".heading").innerHTML = `${data.heading};`
+		// document.querySelector(".description").innerHTML = `${data.description};`
+
+	}
+})
